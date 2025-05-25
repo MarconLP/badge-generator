0d1fc532ae363a8a75e67ac5771efd8619da6de8
MarconLP <13001502+MarconLP@users.noreply.github.com>
2025-05-25 15:59:42 +0200

    prettify

--- src/components/BadgeSelector.tsx
+++ src/components/BadgeSelector.tsx
@@ -1,10 +1,10 @@
-"use client"
- 
-import * as React from "react"
-import { Check, ChevronsUpDown } from "lucide-react"
- 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+"use client";
+
+import * as React from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 import {
   Command,
   CommandEmpty,
@@ -12,13 +12,13 @@ import {
   CommandInput,
   CommandItem,
   CommandList,
-} from "@/components/ui/command"
+} from "@/components/ui/command";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
- 
+} from "@/components/ui/popover";
+
 const frameworks = [
   {
     value: "next.js",
@@ -40,12 +40,12 @@ const frameworks = [
     value: "astro",
     label: "Astro",
   },
-]
- 
+];
+
 export function BadgeSelector() {
-  const [open, setOpen] = React.useState(false)
-  const [value, setValue] = React.useState("")
- 
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
+
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
@@ -72,15 +72,15 @@ export function BadgeSelector() {
                   key={framework.value}
                   value={framework.value}
                   onSelect={(currentValue) => {
-                    setValue(currentValue === value ? "" : currentValue)
-                    setOpen(false)
+                    setValue(currentValue === value ? "" : currentValue);
+                    setOpen(false);
                   }}
                 >
                   {framework.label}
                   <Check
                     className={cn(
                       "ml-auto",
-                      value === framework.value ? "opacity-100" : "opacity-0"
+                      value === framework.value ? "opacity-100" : "opacity-0",
                     )}
                   />
                 </CommandItem>
@@ -90,5 +90,5 @@ export function BadgeSelector() {
         </Command>
       </PopoverContent>
     </Popover>
-  )
+  );
 }
