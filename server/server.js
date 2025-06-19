4a9a293cce4ae9f99b702233d5b2752f0669be59
MarconLP <13001502+MarconLP@users.noreply.github.com>
2025-06-19 21:50:28 +0200

    fix

--- server/server.js
+++ server/server.js
@@ -4,65 +4,55 @@ const path = require('path');
 const PDFDocument = require('pdfkit');
 const fs = require('fs');
 const tickets = require('../data/tickets.json');
-const bodyParser = require('body-parser');
+const customers = require('../data/customer.json');
 const QRCode = require('qrcode');
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express();
 const PORT = 3002;
 
 app.use(express.static('public'));
-
-app.use(bodyParser.json());
+app.use(bodyParser.json())
+app.use(cors())
 
 function sleep(time) {
   return new Promise(resolve => setTimeout(resolve, time));
 }
 
-const getTicketType = (type) => {
-  switch (type) {
-      case 'speaker':
-        return {
-          text: 'SPEAKER',
-          color: '#FF5500'
-        }
-      case 'investor':
-        return {
-          text: 'INVESTOR', 
-          color: '#2E5894'
-        }
-      case 'startup':
-        return {
-          text: 'STARTUP',
-          color: '#1B998B'
-        }
-      case 'regular':
-        return {
-          text: 'STUDENT',
-          color: '#4A4A4A' 
-        }
-      case 'founder':
-        return {
-          text: 'FOUNDER',
-          color: '#FF8C42'
-        }
-      case 'staff':
-        return {
-          text: 'STAFF',
-          color: '#2D3047'
-        }
-      default:
-        return {
-          text: 'REGULAR',
-          color: '#4A4A4A' 
-        }
+const getTicketType = (badge) => {
+  if (badge.ticketName === 'Student') {
+    return {
+      text: 'STUDENT',
+      color: '#4A4A4A' 
     }
+  } else if (badge.ticketName === 'Press') {
+    return {
+      text: 'PRESS',
+      color: '#2E5894'
+    }
+  } else if (badge.ticketName === 'Speaker') {
+    return {
+      text: 'SPEAKER',
+      color: '#FF5500'
+    }
+  } else if (badge.ticketName === 'Staff') {
+    return {
+      text: 'STAFF',
+      color: '#2D3047'
+    }
+  } else if (badge.ticketName === 'Premium' || badge.ticketName === 'Regular') {
+    return {
+      text: badge['extraFields.spezifizierung_normales_ticket'] ?? '',
+      color: badge.ticketName === 'Premium' ? '#FF8C42' : '#4A4A4A'
+    }
+  }
 }
 
 app.post('/api/print', async (req, res) => {
-  // const { data: { scan: { ticketId }} } = await req.body;
-  const ticketId = '6828848d201210c5561b7a0e'
+  const { data: { scan: { ticketId }} } = await req.body;
   const currentBadge = tickets.find((badge) => badge.id === ticketId);
-  const ticketType = getTicketType();
+  const ticketType = getTicketType(currentBadge);
 
   const doc = new PDFDocument({
     size: [273, 380],
@@ -79,11 +69,13 @@ app.post('/api/print', async (req, res) => {
      .text(currentBadge.firstname, 0, 120, { align: 'center' })
      .text(currentBadge.lastname, 0, 150, { align: 'center' });
 
+  const customer = customers.find((customer) => customer.id === currentBadge.customerId);
+  console.log(customer.company)
   // Affiliation
   doc.font('Helvetica')
      .fontSize(20)
      .fillColor('black')
-     .text('WWU MÜNSTER', 0, 200, { align: 'center' });
+     .text(customer.company === '' ? currentBadge['extraFields.an_welcher_uni_studierst_du'] : customer.company, 0, 200, { align: 'center' });
 
   // QR Code
   doc.image(await QRCode.toDataURL(`https://linkedin.com/in/${currentBadge['extraFields.linkedin']}`), 101, 240, { width: 70, align: 'center' });
@@ -94,7 +86,7 @@ app.post('/api/print', async (req, res) => {
   doc.font('Helvetica')
      .fontSize(20)
      .fillColor('white')
-     .text(ticketType.text, 0, 337, { align: 'center' });
+     .text(ticketType.text.toUpperCase(), 0, 337, { align: 'center' });
 
   doc.end();
 
