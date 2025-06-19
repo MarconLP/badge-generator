8f5504be0d3b49138841a0e881ff650f85b30b7e
MarconLP <13001502+MarconLP@users.noreply.github.com>
2025-06-19 19:58:53 +0200

    fix

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const PDFDocument = require('pdfkit');
 const fs = require('fs');
 const tickets = require('../data/tickets.json');
 const bodyParser = require('body-parser');
+const QRCode = require('qrcode');
 
 const app = express();
 const PORT = 3002;
@@ -15,12 +16,53 @@ app.use(bodyParser.json());
 
 function sleep(time) {
   return new Promise(resolve => setTimeout(resolve, time));
-} 
+}
+
+const getTicketType = (type) => {
+  switch (type) {
+      case 'speaker':
+        return {
+          text: 'SPEAKER',
+          color: '#FF5500'
+        }
+      case 'investor':
+        return {
+          text: 'INVESTOR', 
+          color: '#2E5894'
+        }
+      case 'startup':
+        return {
+          text: 'STARTUP',
+          color: '#1B998B'
+        }
+      case 'regular':
+        return {
+          text: 'STUDENT',
+          color: '#4A4A4A' 
+        }
+      case 'founder':
+        return {
+          text: 'FOUNDER',
+          color: '#FF8C42'
+        }
+      case 'staff':
+        return {
+          text: 'STAFF',
+          color: '#2D3047'
+        }
+      default:
+        return {
+          text: 'REGULAR',
+          color: '#4A4A4A' 
+        }
+    }
+}
 
 app.get('/api/print', async (req, res) => {
   // const { data: { scan: { ticketId }} } = await req.body;
-  const ticketId = '6828848d201210c5561b7a03'
+  const ticketId = '6828848d201210c5561b7a0e'
   const currentBadge = tickets.find((badge) => badge.id === ticketId);
+  const ticketType = getTicketType();
 
   const doc = new PDFDocument({
     size: [273, 380],
@@ -44,15 +86,15 @@ app.get('/api/print', async (req, res) => {
      .text('WWU MÜNSTER', 0, 200, { align: 'center' });
 
   // QR Code
-  doc.image('assets/qrcode.png', 101, 240, { width: 70, align: 'center' });
+  doc.image(await QRCode.toDataURL(`https://linkedin.com/in/${currentBadge['extraFields.linkedin']}`), 101, 240, { width: 70, align: 'center' });
 
   // Role Banner
   doc.rect(0, 330, 273, 30)
-     .fill('#FF5500');
+     .fill(ticketType.color);
   doc.font('Helvetica')
      .fontSize(20)
      .fillColor('white')
-     .text('SPEAKER', 0, 337, { align: 'center' });
+     .text(ticketType.text, 0, 337, { align: 'center' });
 
   doc.end();
 
