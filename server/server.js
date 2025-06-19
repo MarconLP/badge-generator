8596972db14913bf914ce6eaa8287d8486235961
MarconLP <13001502+MarconLP@users.noreply.github.com>
2025-06-19 19:38:56 +0200

    fix

--- server/server.js
+++ server/server.js
@@ -29,31 +29,30 @@ app.get('/api/print', async (req, res) => {
 
   doc.pipe(fs.createWriteStream('assets/badge.pdf'));
 
-  // 1. Logo
-  doc.image('assets/startup-contacts.png', 100, 40, { width: 200 });
+  // Logo
+  doc.image('assets/startup-contacts.png', 61, 20, { width: 150 });
 
-  // 3. Participant Name
-  doc.fontSize(40)
-     .text(currentBadge.name, 0, 200, { align: 'center' })
-     .fontSize(30)
-     .text(currentBadge.lastname, { align: 'center' });
+  // Name
+  doc.fontSize(28)
+     .text(currentBadge.firstname, 0, 120, { align: 'center' })
+     .text(currentBadge.lastname, 0, 150, { align: 'center' });
 
-  // 4. Affiliation
+  // Affiliation
   doc.font('Helvetica')
-     .fontSize(24)
+     .fontSize(20)
      .fillColor('black')
-     .text('WWU MÜNSTER', 0, 300, { align: 'center' });
+     .text('WWU MÜNSTER', 0, 200, { align: 'center' });
 
-  // 5. QR Code
-  doc.image('assets/qrcode.png', 100, 350, { width: 200, align: 'center' });
+  // QR Code
+  doc.image('assets/qrcode.png', 101, 240, { width: 70, align: 'center' });
 
-  // 6. Role Banner
-  doc.rect(0, 650, 400, 50)
+  // Role Banner
+  doc.rect(0, 330, 273, 30)
      .fill('#FF5500');
   doc.font('Helvetica')
-     .fontSize(32)
+     .fontSize(20)
      .fillColor('white')
-     .text('SPEAKER', 0, 660, { align: 'center' });
+     .text('SPEAKER', 0, 337, { align: 'center' });
 
   doc.end();
 
